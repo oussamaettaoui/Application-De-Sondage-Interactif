a726d2e93ca3758255698a1de6bc04ff809c6b12
souhailelghi <souhailelghioune98@gmail.com>
2024-05-05 04:27:02 -0700

    Structure folders separating components, services and utilities - Creation de rourtage

---
+++ src/components/Home/CreateSurvey.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CreateSurvey = () => {
+ 
+
+  return (
+    <div>
+      <h2>Créer un nouveau sondage</h2>
+      
+    </div>
+  );
+}
+
+export default CreateSurvey;
---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+  // Exemple d'état pour stocker les sondages disponibles
+  //const [surveys, setSurveys] = useState([]);
+
+  return (
+    <div>
+      <h1>Page d'accueil</h1>
+      {/* Afficher les sondages disponibles */}
+      <ul>
+        
+          <li>test</li>
+      
+      </ul>
+      <button>Créer un nouveau sondage</button>
+    </div>
+  );
+}
+
+export default HomePage;
