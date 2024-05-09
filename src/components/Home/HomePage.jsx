511ec14f0a4e57dce5e2fc4f8e976d9fd0ad2fe4
oussamaettaoui <120644277+oussamaettaoui@users.noreply.github.com>
2024-05-09 12:28:12 +0100

    homePage-coomit

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,8 +1,10 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { demo } from '../../data/data';
+import { Link } from 'react-router-dom';
 function HomePage() {
   const [data,setData] = useState(demo);
   const [search,setSearch] = useState('')
+  useEffect(()=>{},[])
   const surveys = useMemo(()=>{
     return data.filter(e=>e.title.toLowerCase().includes(search.toLowerCase()))
   },[search])
@@ -10,15 +12,19 @@ function HomePage() {
     <div className='w-full h-max'>
       <h1 className='font-bold text-7xl text-center my-8'>Survey Page</h1>
       <div className='w-[35rem] h-max mx-auto my-12'><input type="text" value={search} onChange={e=>setSearch(e.target.value.trimStart())} placeholder='Look for a survey' className='w-[35rem] text-black py-2 px-4 rounded' /></div>
-      <div className='flexBox'>
+      <div className='flexBox flex-col'>
         {surveys.map((survey,i)=>{
           return (
-            <div className='' key={i}>
-              <div className=''>
-                <h1 className=''>{survey.title}</h1>
-                <div className=''>{survey.description}</div>
+            <div className='flex odd:flex-row-reverse justify-center gap-12 px-4 py-2 my-5' key={i}>
+              <div className='w-1/2'>
+                <h1 className='text-2xl font-bold'>{survey.title}</h1>
+                <div className='pt-4 '>{survey.description}</div>
+                <div className='flexBox gap-16 mt-14'>
+                  <Link to=''><div className='primaryEmeraldBtn animationBtn'>Survey Report</div></Link>
+                  <Link to=''><div className='primaryGreenBtn animationBtn'>Answer Survey</div></Link>
+                </div>
               </div>
-              <div className=''><img src={survey.img} alt={survey.title} /></div>
+              <div className='w-96 h-64'><img className='w-full h-full' src={survey.img} alt={survey.title} /></div>
             </div>
           )
         })}
--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,22 @@
 export const demo = [
     {
         title : "Why cats are loved",
-        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde, eaque praesentium doloribus, dicta ex consequuntur modi ad, officiis sapiente impedit iste! Commodi dolorum quas illum id, vitae veritatis! Molestiae impedit eum hic aliquam vitae.",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
         img : '../1.jpg',
     },
     {
-        title : "Why cats are loved",
-        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde, eaque praesentium doloribus, dicta ex consequuntur modi ad, officiis sapiente impedit iste! Commodi dolorum quas illum id, vitae veritatis! Molestiae impedit eum hic aliquam vitae.",
+        title : "Why dogs are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
         img : '../2.jpg',
     },
     {
-        title : "Why cats are loved",
-        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde, eaque praesentium doloribus, dicta ex consequuntur modi ad, officiis sapiente impedit iste! Commodi dolorum quas illum id, vitae veritatis! Molestiae impedit eum hic aliquam vitae.",
+        title : "Why birds are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
         img : '../3.jpg',
     },
     {
-        title : "Why cats are loved",
-        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde, eaque praesentium doloribus, dicta ex consequuntur modi ad, officiis sapiente impedit iste! Commodi dolorum quas illum id, vitae veritatis! Molestiae impedit eum hic aliquam vitae.",
+        title : "Why snakes are loved",
+        description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eaque cum nihil sunt distinctio labore maiores quas, deleniti fugiat eius, exercitationem praesentium. Quia neque magni eius architecto maiores ea praesentium!Suscipit corrupti illo veritatis unde,",
         img : '../4.jpg',
     }
 ]
