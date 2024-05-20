860ba3ba8cae00a622f887c5dafdaba3c74dd561
Amine <mezzatamine2002@gmail.com>
2024-05-20 16:06:20 +0100

    push

--- src/components/SurveyReport.jsx
+++ src/components/SurveyReport.jsx
@@ -3,19 +3,30 @@ import { useFetch } from '../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 
 function SurveyReport() {
-
-
   const {data, loading} = useFetch();
-  const [filteredData, setFilteredData] = useState(data);
+  const [filteredData, setFilteredData] = useState({});
   const {id} = useParams();
-  console.log(data);
+  useEffect(() => {
+    if (!loading && data.length > 0 ){
+      const fdata = data.find(dt => dt.id === id);
+      setFilteredData(fdata);
+    }
+  },[data, loading, id])
+  //console.log("I am initial data :", data);
+  console.log("I am the filtered data :", filteredData);
+  /*const fdata = data.filter(data => data.id === id);
+  setFilteredData(fdata)
+  console.log(filteredData);*/
+
   return (
     <>
-    {
-      <h1> Report</h1>
-    }
+    <div style={{marginTop: "6rem"}} className="">
+      <h1>I am the survey</h1>
+      <div>{filteredData.title}</div>
+      <div>{filteredData.description}</div>
       
-
+    </div>
+      
     </>
   );
 }
