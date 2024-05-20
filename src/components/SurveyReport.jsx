f14f3410470cc25a2653b32bdbd86a03de2a12b4
Amine <mezzatamine2002@gmail.com>
2024-05-20 14:37:05 +0100

    creating the survey report page

--- src/components/SurveyReport.jsx
+++ src/components/SurveyReport.jsx
@@ -1,8 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
-function SurveyReport({ survey }) {
+function SurveyReport() {
+
+
+  const {data, loading} = useFetch();
+  const [filteredData, setFilteredData] = useState(data);
+  const {id} = useParams();
+  console.log(data);
   return (
     <>
+    {
+      <h1> Report</h1>
+    }
+      
 
     </>
   );
